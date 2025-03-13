d4b7264f7e4ba88b96ffcbf35ab263f8cd07f041
unicorn-phenex-RX-0 <daisuke_dragons@outlook.com>
2025-03-13 20:57:23 +0900

    Update music.js

--- js/music.js
+++ js/music.js
@@ -7,12 +7,14 @@ const btngogo = document.getElementById('gogo');
 const btnrezero = document.getElementById('rezero');
 const btnimpact = document.getElementById('impact');
 const btn3000fever = document.getElementById('3000fever');
+const btngogo2 = document.getElementById('gogo2');
 
 //イベントリスナー
 btngogo.addEventListener('click',gogo);
 btnrezero.addEventListener('click',rezero);
 btnimpact.addEventListener('click',impact);
 btn3000fever.addEventListener('click',fever3000);
+btngogo2.addEventListener('click',gogo2);
 
 function gogo(){
 	const audio = new Audio('music/gogo.mp3');
@@ -35,5 +37,13 @@ function impact(){
 function fever3000(){
 	const audio = new Audio('music/3000fever.mp3');
 	audio.play();
+}
+
+function gogo2(){
+	const audio = new Audio('music/gogo.mp3');
+        let random = Math.floor(Math.random() * 65536)+1;
+	if(random>=64880){
+	audio.play();
+	}
 }
 
